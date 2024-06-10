3cc4ae5a6639ec6b128f024e3abb12a59d394a9a
Berenu <sudosuberenu@gmail.com>
2024-06-10 16:17:19 +0200

    Fix mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,25 +14,20 @@ export default function Home() {
     <main className="flex flex-col min-h-screen bg-[#435D52]  text-white">
       <div className="bg-[#0f1a1f] w-full p-5 text-black text-sm text-right">
         <Button
-          className="rounded-sm mr-2"
+          className="rounded-sm mr-2 text-black bg-[#50d2c1] hover:bg-[#33877c]"
           variant="contained"
-          style={{
-            color: "black",
-            backgroundColor: "#50d2c1",
-            boxShadow: "none",
-            fontSize: "0.75rem",
-          }}
         >
           Connect wallet
         </Button>
       </div>
 
-      <div className="bg-[#50d2c1] w-full p-2 text-black text-sm">
+      <div className="bg-[#50d2c1] w-full p-2 text-black text-sm flex justify-between">
         {" "}
         Welcome to the Liquidity Bet Hook{" "}
+        <span className="font-bold">⚠️ Attention: Mocked FE</span>
       </div>
 
-      <div className="flex gap-x-5 p-5 ">
+      <div className="flex gap-x-5 p-5 hidden md:flex">
         <div className="w-2/4 flex flex-col gap-y-5">
           <PoolSelection />
           <CurrentJackpot />
@@ -43,6 +38,16 @@ export default function Home() {
         </div>
         <div className="w-1/4 flex flex-col gap-y-5">
           <FormPosition />
+        </div>
+      </div>
+
+      <div className="flex flex-col p-5 md:hidden gap-y-5">
+        <div className="flex flex-col gap-y-5">
+          <PoolSelection />
+          <CurrentJackpot />
+          <FormPosition />
+          <LiquidityCandleChart />
+          <Trades />
         </div>
       </div>
 
